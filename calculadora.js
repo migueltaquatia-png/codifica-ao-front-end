ad84385ab75e5d48f7c9ed7789bc1a8731b3bf80
MIGUEL ROSA TAQUATIÁ <miguel_taquatia@estudante.sesisenai.org.br>
2026-09-09 14:31:44 -0300

    atividade soma 09/09

---
+++ calculadora.js
@@ -0,0 +1,6 @@
+let numero1 = Number(prompt("Digite o primeiro número:"));
+let numero2 = Number(prompt("Digite o segundo número:"));
+let resultado = numero1 + numero2;
+alert("O resultado da soma é: " + resultado);
+
+// soma de dois numeros ta prof dia 9/09/26
