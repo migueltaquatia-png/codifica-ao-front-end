82fed80b84cdacd698a10c07fe4c3608edd7d4d9
MIGUEL ROSA TAQUATIÁ <miguel_taquatia@estudante.sesisenai.org.br>
2026-09-09 17:19:45 -0300

    atividade 1

---
+++ 00-demo/src/App.jsx
@@ -0,0 +1,50 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="cont-App">
+      <h1>estudando com react</h1>
+      <h2>onde posso trabalhar como desenvolvedor?</h2>
+      <p>
+        Empresas e Software Houses: Consultorias e fábricas de software que desenvolvem aplicações sob demanda para terceiros.
+        </p>
+
+        <p>
+          Startups de Tecnologia: Ambientes dinâmicos que valorizam a agilidade do perfil técnico para construir MVPs (produtos mínimos viáveis) e novas funcionalidades.
+        </p>
+
+        <p>
+          Departamentos de TI e Inovação: Grandes empresas de setores tradicionais (bancos, varejo, saúde, indústria) que mantêm equipes internas para cuidar de seus portais e sistemas.
+        </p>
+
+        <p>
+          Agências Digitais e de Marketing: Criação e manutenção de e-commerces, landing pages interativas, hotsites e blogs corporativos.
+        </p>
+        <p>
+          Setor Público: Órgãos governamentais e universidades por meio de estágios, contratos temporários ou concursos públicos para cargos técnicos.
+        </p>
+
+        <h2>
+          trabalhos com tcnologia mas sem programaçao
+        </h2>
+        <p>
+          Gostar de tecnologia sem ter afinidade com código não é um obstáculo. O mercado de TI é gigante e depende fortemente de funções que focam em pessoas, processos, design, dados e negócios.
+        </p>
+
+        <ul>
+          <li>Product Management & Negócios</li>
+          <li>Design e Experiência do Usuário (UI/UX)</li>
+          <li>Gestão de Projetos e Agilidade</li>
+          <li>Gestão de Projetos e Agilidade</li>
+          <li>Vendas Técnicas e Sucesso do Cliente</li>
+        </ul>
+        
+
+    </div>
+  )
+}
+
+export default App
